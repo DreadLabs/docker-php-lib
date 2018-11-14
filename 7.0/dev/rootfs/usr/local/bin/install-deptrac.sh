9a6c1f0b6bfd75c4b71e86062be7eab4c83c922c
Thomas Juhnke <tommy@van-tomas.de>
2018-11-14 19:40:39 +0100

    Bump deptrac to 0.3.0

--- 7.0/dev/rootfs/usr/local/bin/install-deptrac.sh
+++ 7.0/dev/rootfs/usr/local/bin/install-deptrac.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl -LS http://get.sensiolabs.de/deptrac.phar -o /usr/local/bin/deptrac
+curl --location --show-error https://github.com/sensiolabs-de/deptrac/releases/download/0.3.0/deptrac.phar -o /usr/local/bin/deptrac
 
 chmod +x /usr/local/bin/deptrac
